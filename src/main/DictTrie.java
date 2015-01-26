af289757ec4ea50ae4b46f1fd1b60bcb325a58bb
Chris <ckerich@gmail.com>
2015-01-26 12:17:03 -0500

    First draft of trie for the dictionary

---
+++ src/main/DictTrie.java
@@ -0,0 +1,52 @@
+package main;
+
+import java.util.HashMap;
+
+public class DictTrie {
+
+	private boolean isWord;
+	private String word;
+	private char letter;
+	private HashMap<Character, DictTrie> children;
+	
+	public DictTrie(char letter)
+	{
+		this.letter = letter;
+		this.children = new HashMap<Character, DictTrie>();
+	}
+	
+	public boolean isWord()
+	{
+		return isWord;
+	}
+	
+	public String getWord()
+	{
+		if(isWord())
+			return word;
+		else
+			return null;
+	}
+	
+	public char getLetter()
+	{
+		return letter;
+	}
+	
+	public void setWord(String word)
+	{
+		isWord = true;
+		this.word = word;
+	}
+	
+	public DictTrie getChild(char letter)
+	{
+		return children.get(letter);
+	}
+	
+	public void addChild(char letter, DictTrie child)
+	{
+		children.put(letter, child);
+	}
+	
+}
